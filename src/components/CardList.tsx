64751d9c3fb0ece23ab4127a940c97ad6391324f
Alex Bauruel <abauruel@gmail.com>
2021-07-02 19:08:55 -0300

    feat: add features

--- src/components/CardList.tsx
+++ src/components/CardList.tsx
@@ -17,16 +17,30 @@ interface CardsProps {
 
 export function CardList({ cards }: CardsProps): JSX.Element {
   // TODO MODAL USEDISCLOSURE
+  const {isOpen, onClose, onOpen} = useDisclosure()
 
   // TODO SELECTED IMAGE URL STATE
-
+ const [imageUrl, setImageUrl] = useState("")
+ 
   // TODO FUNCTION HANDLE VIEW IMAGE
+  function handleViewImage(url: string){
+    
+    setImageUrl(url)
+    onOpen()
+  }
 
   return (
     <>
-      {/* TODO CARD GRID */}
+      <SimpleGrid columns={3} spacing={40}>
+        {cards?.map((card, index) => (
+          <Card 
+          key={index}
+            data={card} 
+            viewImage={()=>handleViewImage(card.url)}/>
+        ))}
+      </SimpleGrid>
 
-      {/* TODO MODALVIEWIMAGE */}
+      {<ModalViewImage isOpen={isOpen} onClose={onClose} imgUrl={imageUrl}  />}
     </>
   );
 }
